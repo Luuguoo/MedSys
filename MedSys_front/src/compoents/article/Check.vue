47392688771be3f47dcf943a64e5d0cd544035f1
saber <62688714+wzx666-yf@users.noreply.github.com>
2024-05-28 20:40:34 +0800

    后台的文章模块前端代码

---
+++ MedSys_front/src/compoents/article/Check.vue
@@ -0,0 +1,178 @@
+<template>
+  <div>
+    <el-card class="box-card" shadow="always" style="border-radius: 20px;">
+      <div slot="header" class="clearfix">
+        <p align="left" style="font-size: 20px">审核文章</p>
+      </div>
+      <div style="min-height: 600px">
+        <el-row>
+          <el-col :span="24">
+            <el-card>
+              <el-row :gutter="20">
+                <el-col :span="5">
+                  <el-select v-model="tid" placeholder="请选择">
+                    <el-option
+                        v-for="item in atypes"
+                        :key="item.tid"
+                        :label="item.tname"
+                        :value="item.tid">
+                    </el-option>
+                  </el-select>
+                </el-col>
+                <el-col :span="5">
+                  <el-input v-model="title" placeholder="标题"></el-input>
+                </el-col>
+                <el-col :span="5">
+                  <el-input v-model="lname" placeholder="发布人"></el-input>
+                </el-col>
+                <el-col :span="6">
+                  <el-button icon="el-icon-search" @click="paging(1)">搜索</el-button>
+                </el-col>
+              </el-row>
+            </el-card>
+            <el-table :data="page.list" stripe style="width: 100%;">
+              <el-table-column prop="atype.tname" label="用户名" width=""></el-table-column>
+              <el-table-column prop="title" label="文章标题" width=""></el-table-column>
+              <el-table-column label="文章图片">
+                <template slot-scope="scope">
+                  <img :src="scope.row.photo" width="80px">
+                </template>
+              </el-table-column>
+              <el-table-column prop="lname" label="发布人" width=""></el-table-column>
+              <el-table-column prop="atime" label="发布时间" width=""></el-table-column>
+              <el-table-column prop="flag" label="审核状态" width=""></el-table-column>
+              <el-table-column width="400px">
+                <template slot-scope="scope">
+                  <el-button type="primary" icon="el-icon-more-outline" size="mini" circle
+                             @click="toDetail(scope.row)"></el-button>
+                  <el-button type="warning" size="mini" round
+                             @click="check(scope.row.aid,'审核通过')">审核通过
+                  </el-button>
+                  <el-button type="info" size="mini" round
+                             @click="check(scope.row.aid,'审核不通过')">审核不通过
+                  </el-button>
+                  <el-button type="danger" icon="el-icon-delete" size="mini" circle
+                             @click="del(scope.row.aid)"></el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+            <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page="page.pageNum"
+                :page-sizes="[2,3,4,5]"
+                :page-size="page.pageSize"
+                layout="total,sizes,prev,pager,next,jumper"
+                :total="page.total"></el-pagination>
+          </el-col>
+        </el-row>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      atypes: [],
+      page: {},
+      flag: '待审核',
+      pageSize: 3,
+      lname: '',
+      title: '',
+      tid: 0,
+      visible: [],
+    }
+  },
+  methods: {
+    paging(curPage) {
+      this.$axios.get('back/article/list?curPage=' + curPage + '&flag=' + this.flag + '&tid=' + this.tid + '&lname=' + this.lname + '&title=' + this.title + '&pageSize=' + this.pageSize)
+          .then(resp => {
+            this.page = resp.data.data
+            console.log(resp.data.data)
+          })
+    },
+    toDetail(article) {
+      this.$router.push('/backHome/backarticleDetail?article=' + JSON.stringify(article) + '&page=check')
+    },
+    check(aid,checkResult){
+      this.$axios.get('back/article/check?aid=' + aid+'&checkResult='+checkResult)
+          .then(resp => {
+            if (resp.data.code === 1) {
+              this.$message({
+                type: 'success',
+                message: resp.data.msg
+              });
+            } else {
+              this.$message.error(resp.data.msg);
+            }
+            this.paging(1)
+          })
+    },
+    checkNoPass(aid){
+      this.$axios.get('back/article/check?aid=' + aid+'&checkResult=审核通过')
+          .then(resp => {
+            if (resp.data.code === 1) {
+              this.$message({
+                type: 'success',
+                message: resp.data.msg
+              });
+            } else {
+              this.$message.error(resp.data.msg);
+            }
+            this.paging(1)
+          })
+    },
+    del(aid) {
+      this.$confirm('此操作将永久删除该文记录, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$axios.get('back/article/del?aid=' + aid)
+            .then(resp => {
+              if (resp.data.code === 1) {
+                this.$message({
+                  type: 'success',
+                  message: resp.data.msg
+                });
+              } else {
+                this.$message.error(resp.data.msg);
+              }
+              this.paging(1)
+              // this.paging(this.page.pages==1?1:(this.page.pageNum==this.page.pages?this.page.pageNum-1:this.page.pageNum))
+            })
+
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
+    },
+    handleSizeChange(newSize) {
+      this.pageSize = newSize
+      this.paging(1)
+    },
+    handleCurrentChange(newPage) {
+      this.paging(newPage)
+    },
+    getAtypeList() {
+      this.$axios.get('back/atype/findAll')
+          .then(resp => {
+            this.atypes = resp.data.data
+            this.atypes.unshift({tid: 0, tname: '全部'})
+          })
+    }
+  },
+  created() {
+    this.paging(1)
+    this.getAtypeList()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
